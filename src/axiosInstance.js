4167ede7e1442d22a8a8cca8ec32798249616cdc
Apple <rohithuta.p@gmail.com>
2025-05-16 11:49:51 +0530

    Axios - prod

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 axios.defaults.withCredentials = true;
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:8000",
-    // baseURL: "https://danceclassdashboard-api.onrender.com",
+    // baseURL: "http://localhost:8000",
+    baseURL: "https://danceclassdashboard-api.onrender.com",
 })
 axiosInstance.interceptors.request.use((config) => {
     const token = localStorage.getItem('token');
